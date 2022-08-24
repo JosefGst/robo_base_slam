83659aa3c0c4fbab5a42b46995690281acb8f34a
jgstoettner <jgstoettner@lscm.hk>
2022-08-24 13:34:03 +0800

    cartographer maps

--- shell/map_write.sh
+++ shell/map_write.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rosservice call /finish_trajectory 0
-rosservice call /write_state "filename: '${HOME}/ros/robo_base_ws/src/robo_base_slam/maps/cartographer.pbstream'"
-rosrun cartographer_ros cartographer_pbstream_to_ros_map -map_filestem=${HOME}/ros/robo_base_ws/src/robo_base_slam/maps/cartographer -pbstream_filename=${HOME}/ros/robo_base_ws/src/robo_base_slam/maps/cartographer.pbstream -resolution=0.05
+rosservice call /write_state "filename: '${HOME}/ros/robo_base_ws/src/robo_base_slam/maps/cartographer3d.pbstream'"
+rosrun cartographer_ros cartographer_pbstream_to_ros_map -map_filestem=${HOME}/ros/robo_base_ws/src/robo_base_slam/maps/cartographer3d -pbstream_filename=${HOME}/ros/robo_base_ws/src/robo_base_slam/maps/cartographer3d.pbstream -resolution=0.05
